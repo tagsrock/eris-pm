7369565669d0b28cfd1b5a5492faabfbce0a6f1b
Casey Kuhlman <casey@erisindustries.com>
2015-12-11 04:26:24 +0000

    derp, should be %s

--- perform/jobs_test_jobs.go
+++ perform/jobs_test_jobs.go
@@ -61,7 +61,7 @@ func QueryContractJob(query *definitions.QueryContract, do *definitions.Do) (str
 	}
 
 	// Finalize
-	logger.Printf("Return Value =>\t\t\t%X\n", result)
+	logger.Printf("Return Value =>\t\t\t%s\n", result)
 	return result, nil
 }
 
@@ -78,7 +78,7 @@ func QueryAccountJob(query *definitions.QueryAccount, do *definitions.Do) (strin
 	}
 
 	// Result
-	logger.Printf("Return Value =>\t\t\t%X\n", result)
+	logger.Printf("Return Value =>\t\t\t%s\n", result)
 	return result, nil
 }
 
@@ -94,7 +94,7 @@ func QueryNameJob(query *definitions.QueryName, do *definitions.Do) (string, err
 		return "", err
 	}
 
-	logger.Printf("Return Value =>\t\t\t%X\n", result)
+	logger.Printf("Return Value =>\t\t\t%s\n", result)
 	return result, nil
 }
 
@@ -111,7 +111,7 @@ func QueryValsJob(query *definitions.QueryVals, do *definitions.Do) (string, err
 		return "", err
 	}
 
-	logger.Printf("Return Value =>\t\t\t%X\n", result)
+	logger.Printf("Return Value =>\t\t\t%s\n", result)
 	return result, nil
 }
 
