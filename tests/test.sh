d31c6b1e032c3a496c07332f8ad5c1fd01c81697
Casey Kuhlman <casey@erisindustries.com>
2016-01-10 19:13:13 +0100

    close quotes on jq command

--- tests/test.sh
+++ tests/test.sh
@@ -95,7 +95,7 @@ test_setup(){
   echo -e "Backup Key =>\t\t\t$key2_addr"
 
   # fixup the genesis.json with the new addresses
-  jq ".accounts[0].address=\"$key1_addr\" | .accounts[1].address=\"$key2_addr" tests/fixtures/chaindata/genesis.json.example > tests/fixtures/chaindata/genesis.json
+  jq ".accounts[0].address=\"$key1_addr\" | .accounts[1].address=\"$key2_addr\"" tests/fixtures/chaindata/genesis.json.example > tests/fixtures/chaindata/genesis.json
 
   # start the chain with the current genesis.json
   eris chains new epm-tests-$uuid --dir tests/fixtures/chaindata #1>/dev/null
